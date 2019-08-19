fd6c8e867036f4aa87561bd4463af1e5bee5f113
ninjat <hotanya.r@gmail.com>
2019-08-19 21:53:58 +1200

    now also retrieves AMP NZ Shares and Property

--- scraper.py
+++ scraper.py
@@ -6,14 +6,24 @@ from bs4 import BeautifulSoup
 #can make this better by having a list of random header
 example_headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
 
-#page = requests.get("https://www.vanguardinvestments.com.au/adviser/adv/investments/product.html#/fundDetail/wholesale/portId=8122/?portfolio")
-page = requests.get("https://www.bloomberg.com/quote/VAN1579:AU",headers=example_headers) #need to have headers to avoid being blocked
-#print(page.content)
-soup = BeautifulSoup(page.content, 'html.parser')
-find = soup.find_all("span",class_='priceText__1853e8a5')[0].get_text()
-print(find)
+#vanguard
+vanguard = requests.get("https://www.bloomberg.com/quote/VAN1579:AU",headers=example_headers) #need to have headers to avoid being blocked
+soup = BeautifulSoup(vanguard.content, 'html.parser')
+vanguard_price = soup.find_all("span", class_='priceText__1853e8a5')[0].get_text()
+print("Vanguard Price:\n" + vanguard_price+"AUD")
+
+#AMP Capital NZ Index 
+ampnzshares = requests.get("https://www.ampcapital.com/nz/en/investments/funds/index-funds/nz-shares-index-fund",headers=example_headers) #need to have headers to avoid being blocked
+soup1 = BeautifulSoup(ampnzshares.content, 'html.parser')
+ampnzshares_price = soup1.find_all("div", class_='ht-align_center fund_detail_panels-panel-cell')[0].get_text()
+print("AMP Capital NZ Index Price:\n" + ampnzshares_price+"NZD")
+
+#AMP Capital Australisian Property Index Fund
+ampAustralisianProperty = requests.get("https://www.ampcapital.com/nz/en/investments/funds/index-funds/australasian-property-index-fund",headers=example_headers) #need to have headers to avoid being blocked
+soup2 = BeautifulSoup(ampAustralisianProperty.content, 'html.parser')
+ampAustralianProperty_price = soup2.find_all("div", class_='ht-align_center fund_detail_panels-panel-cell')[0].get_text()
+print("AMP Capital Australisian Property Index Fund:\n" + ampAustralianProperty_price+"NZD")
 
 
-#print(list(soup.children)[3])
-#print(soup.prettify())
+
 
