bfecef158081f11300346cef7dd3f4520307d6f7
ninjat <hotanya.r@gmail.com>
2019-08-19 23:07:55 +1200

    gets current portfolio value + prices

--- scraper.py
+++ scraper.py
@@ -2,9 +2,32 @@
 
 import requests
 from bs4 import BeautifulSoup
+from datetime import date, timedelta
+import datetime
 
-#can make this better by having a list of random header
-example_headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+def CurrentPortfolioValue():
+    initial_porfolio_price = 100
+    initial_date = date(2018, 8, 19)
+    fortnightly_contrib = 50
+    current_value = 0
+    #print(initial_date)
+
+    #get day difference between today and initial date
+    current_date = date.today()
+    days_difference = (current_date - initial_date).days #.days shows just the number of days
+    #print(days_difference)
+
+    count=days_difference
+    while count > 14: #while difference is less than 14 days i.e forntightly payment hasnt been made
+        current_value += initial_porfolio_price + fortnightly_contrib
+        count = count - 14
+    print("Current Portfolio value is $"+str(current_value))
+
+CurrentPortfolioValue() #retrieves current portfolio value
+
+#sets a user-agent    
+example_headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}#can make this better by having a list of random header
+
 
 #vanguard
 vanguard = requests.get("https://www.bloomberg.com/quote/VAN1579:AU",headers=example_headers) #need to have headers to avoid being blocked
